2956335c847adef82200bcfb56ce88b1ca84c377
timofeya123 <timofeya123@inbox.ru>
2021-05-20 22:32:54 +0600

    GrayScaleImageEx_comment

--- src/com/AWT/GrayScaleImageEx.java
+++ src/com/AWT/GrayScaleImageEx.java
@@ -12,45 +12,46 @@ import javax.swing.JPanel;
 
 class GrayScaleImageSurface extends JPanel {
 
+    // переменная для картинки
     private Image enotIMG;
     private BufferedImage bufimg;
     private Dimension d;
 
-    public GrayScaleImageSurface() {
-        loadImage();
-        determineAndSetSize();
-        createGrayImage();
+    public GrayScaleImageSurface() { // конструктор
+        loadImage(); // загрузка
+        determineAndSetSize(); // размеры
+        createGrayImage(); // создание серого оттенка
     }
 
-    private void determineAndSetSize() {
+    private void determineAndSetSize() { // установка размеров
         d = new Dimension();
         d.width = enotIMG.getWidth(null);
         d.height = enotIMG.getHeight(null);
         setPreferredSize(d);
     }
 
-    private void createGrayImage() {
+    private void createGrayImage() { // создание маски
         bufimg = new BufferedImage(d.width, d.height,
                 BufferedImage.TYPE_BYTE_GRAY);
 
-        Graphics2D g2d = bufimg.createGraphics();
+        Graphics2D g2d = bufimg.createGraphics(); // прорисовка графики
         g2d.drawImage(enotIMG, 0, 0, null);
         g2d.dispose();
     }
 
     private void loadImage() {
         enotIMG = new ImageIcon("enot.jpeg").getImage();
-    }
+    } // создаём картинку из файла
 
-    private void doDrawing(Graphics g) {
-        Graphics2D g2d = (Graphics2D) g;
-        g2d.drawImage(bufimg, null, 0, 0);
+    private void doDrawing(Graphics g) { // прорисовка эл.
+        Graphics2D g2d = (Graphics2D) g; // графика
+        g2d.drawImage(enotIMG, 0, 0, null); // картинка
     }
 
     @Override
-    public void paintComponent(Graphics g) {
+    public void paintComponent(Graphics g) { // зарисовка компонента
         super.paintComponent(g);
-        doDrawing(g);
+        doDrawing(g); // вызов прорисовки компонента
     }
 }
 
@@ -58,27 +59,27 @@ public class GrayScaleImageEx extends JFrame {
 
     public GrayScaleImageEx() {
         initUI();
-    }
+    }// конструктор с инициализацией
 
-    private void initUI() {
+    private void initUI() { // инициализация
 
-        GrayScaleImageSurface dpnl = new GrayScaleImageSurface();
+        GrayScaleImageSurface dpnl = new GrayScaleImageSurface(); // наш объект
         add(dpnl);
 
         pack();
 
-        setTitle("GrayScale image");
+        setTitle("GrayScale image"); // имя
         setLocationRelativeTo(null);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // выход
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {// главный класс
 
-        EventQueue.invokeLater(new Runnable() {
+        EventQueue.invokeLater(new Runnable() { // поток
             @Override
-            public void run() {
-                GrayScaleImageEx ex = new GrayScaleImageEx();
-                ex.setVisible(true);
+            public void run() { // запуск
+                GrayScaleImageEx ex = new GrayScaleImageEx();// создание нашего класса
+                ex.setVisible(true); // видимость
             }
         });
     }
